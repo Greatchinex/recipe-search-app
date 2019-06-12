25a7150e60a9bf037094e020560e285c84e63522
Chilaka Chinedu <greatchinex@gmail.com>
2019-06-12 15:01:58 +0100

    Updates

---
+++ precache-manifest.5d24e7d118dd67788ac9e9725895d16c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2aac1673a37bc20f6d95425b231d4da2",
+    "url": "/recipe-search-app/index.html"
+  },
+  {
+    "revision": "0be62e405c0940abedb7",
+    "url": "/recipe-search-app/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "12cebc3cb09e4439a9ed",
+    "url": "/recipe-search-app/static/css/main.1ab9420d.chunk.css"
+  },
+  {
+    "revision": "0be62e405c0940abedb7",
+    "url": "/recipe-search-app/static/js/2.727ab3ae.chunk.js"
+  },
+  {
+    "revision": "12cebc3cb09e4439a9ed",
+    "url": "/recipe-search-app/static/js/main.7a43c993.chunk.js"
+  },
+  {
+    "revision": "1540a412136f2b44cab9",
+    "url": "/recipe-search-app/static/js/runtime~main.3a88d416.js"
+  }
+]);
